35dabb9424ab8ab7b16af573f458d59a030b2853
Yong <yong.southa@gmail.com>
2019-09-17 09:51:53 +0200

    new update

---
+++ app.js
@@ -0,0 +1,29 @@
+let bouton_menu = document.querySelector(".menu-burger");
+let menu = document.querySelector("header nav");
+document.addEventListener('click', affichermenu)
+
+bouton_menu.addEventListener("click",menuselect);
+
+
+function menuselect()
+{
+    menu.classList.add("active");
+}
+
+
+function affichermenu()
+{
+    var menu = document.querySelector('header nav');
+
+    //console.log(event.target);
+    if (!event.target.matches('.menu-burger') && !event.target.matches('header nav')){
+        //console.log("That's not a burgermenu and nav");
+        if(menu.classList.contains('active')) {
+            menu.classList.remove("active")
+        }
+
+        }
+    return;
+}
+
+

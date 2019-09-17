9e647f562ca1d9834133a8004d9154add33522c5
Yong <yong.southa@gmail.com>
2019-09-17 16:17:07 +0200

    slt les potes

--- sw.js
+++ sw.js
@@ -1,64 +1,101 @@
 const version = "7";
 
-let fichier_offline = [
-    '/index.html',
-    '/style.css',
-    '/reset.css',
-    '/app.js'
-
+let fichiers_offline = [
+  "/index.html",
+  "/style.css",
+  "/reset.css",
+  "/app.js"
 ]
 
-self.addEventListener("install",function(event){
-    console.warn("[SW] ver "+version+" - Install Event ");
-    self.skipWaiting()
-    event.waitUntil(
-        caches.open("cache-v1").then(function(cache){
-            console.log("Mise en cache");
-            return cache.addAll(fichier_offline)
-        })
-        .catch(function(erreur){
-            console.log(erreur);
-        })
+self.addEventListener("install",function(event) {
+  console.warn("[SW] V."+version+" - INSTALL EVENT");
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open("cache-v1").then(function(cache) {
+      console.log("[PWA] Caching pages during install");
+      return cache.addAll(fichiers_offline);
+    })
+  )
+})
+
+self.addEventListener("activate",function(event) {
+  console.warn("[SW] V."+version+" - ACTIVATE EVENT");
+  event.waitUntil(self.clients.claim());
+})
+
+self.addEventListener("fetch",function(event) {
+  console.warn("[SW] V."+version+" - FETCH EVENT");
+  console.log(event.request);
+
+  event.respondWith(
+    fromCache(event.request).then(
+      function(response) {
+        console.warn("Il y a bien correspondance dans le cache "+event.request.url);
+
+        // On appel le serveur pour charger la nouvelle version du fichier
+        // pour la prochaine fois
+        event.waitUntil(
+          fetch(event.request).then(function(response) {
+            console.log("maj cache pour prochain chargement");
+            return updateCache(event.request, response);
+          })
+        )
+
+        return response;
+      },
+      function(echec) {
+
+        console.warn("Pas de fichier en cache... On va chercher en ligne sur le serveur avec fetch "+event.request.url);
+        return fetch(event.request)
+          .then(function(response) {
+            // C'est un succès
+           // event.waitUntil(updateCache(event.request, response.clone()));
+
+            return response;
+          })
+          .catch(function(error) {
+            console.log(
+              "Pas de cache et pb de réseau : " + error
+            );
+          });
+      }
     )
-
+  )
 })
 
-self.addEventListener("activate",function(event){
-    console.warn("[SW] ver - Activate Event");
+/* Fonctions Gestion Cache */
 
-})
+function fromCache(request) {
+  // Vérifier si le fichier est en cache (cache.match)
+  // Retourne la réponse
+  // Sinon reject...
+  return caches.open("cache-v1").then(function(cache) {
+    return cache.match(request).then(function(matching) {
+      console.log(matching)
+      if (!matching || matching.status === 404) {
+        return Promise.reject("no-match");
+      }
 
-self.addEventListener("fetch",function(event){
-    console.warn("[SW] - Fetch Event");
-    console.log(event.request);
-  //  event.respondWith;
-    /*if(event.request.url == "http://localhost:5000/page2.html"){
-        event.respondWith(
-            new Response("Unlucky requête interceptée et modifiée")
-        )
-    }*/
-        event.respondWith(
-            fromcache(event.request).then(function(response){
-                console.warn("Il y a correspondance avec le cache "+event.request.url);
-                return response
-            })
-            .catch(function(response){
-                console.warn("Pas de fichier en cache... on va chercher en ligne sur le serveur");
-                return fetch(event.request)
-            })
-        )
-})
-
-//récupérer des fichiers en cache
-function fromcache(request){
-   return caches.open("cache-v1").then(function(cache){
-        return caches.match(request).then(function(matching){
-            if (!matching || matching.status == 404){
-                return Promise.reject("no-match mec")
-            }
-            else{
-                return matching;
-            }
-        });
+      return matching;
     });
+  });
 }
+
+
+function updateCache(request, response) {
+  console.log(request);
+  console.log(response);
+  return caches.open("cache-v1").then(function(cache) {
+    return cache.put(request, response);
+  });
+}
+
+
+self.addEventListener('push', function (ev) {
+  ev.waitUntil(
+    self.registration.showNotification('My Title', {
+      body: 'Hello world!'
+    })
+  );
+})
